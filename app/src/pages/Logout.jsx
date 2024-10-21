c043c8208e1ab56248bb723ad63118147aa72bdb
lionardus87 <134745829+lionardus87@users.noreply.github.com>
2024-10-21 05:56:28 +0000

    adding firebase and signup page

---
+++ app/src/pages/Logout.jsx
@@ -0,0 +1,16 @@
+// import {signOut} from "firebase/auth"
+// import {useEffect} from "react"
+
+// export function Logout(props){
+//     const navigate = useNavigate()
+//     useEffect(() =>)
+//         signOut(props.authapp)
+//         .then(() => {
+//             // take the user back to homepage
+//             navigate("/")
+//         })
+//         .catch((error) => {console.log(error)})
+//     return(
+//         <div></div>
+//     )
+// }
---
+++ app/src/pages/Signin.jsx
@@ -0,0 +1,20 @@
+
+//import Button 'react-bootstrap/Button';
+// import Form from ' react-bootstrap/Form';
+// import Container from ' react-bootstrap/Container' 
+// import Row from 'react-bootstrap/Row'
+// import Col from ' react-bootstrap/Col'
+
+// import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+// export function Signup ( props ) {
+//     const signUpUser = ( event ) => {
+//         event.preventDefault)
+//         const formdata = new FormData (event. target)
+//         const email = formata. get ("email")
+//         const password = formdata.get ("password")
+//         createUserWithEmailAndPassword( props. authapp, enail, password )
+//         •then ( (response) => console. log (response) )
+//         •catch ( (error) => console. log(error) )
+//     }
+//     return (
